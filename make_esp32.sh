ee8f3c3f767fffb2b2373bb53012c18e5aeca97d
Jos Verlinde <jos_verlinde@hotmail.com>
2020-05-04 22:25:04 +0200

    build automation

--- make_esp32.sh
+++ make_esp32.sh
@@ -8,23 +8,69 @@ NC='\x1B[0m' # No Color
 echo -e  "${CYAN}ESP build process${NC}"
 
 #Locations 
+ESP_DIR=$HOME/esp32
+
 export MPY_DIR=/mnt/c/develop/MyPython/micropython
-export ESPIDF=$HOME/src/github.com/espressif/esp-idf  # Or any path you like.
+export ESPIDF=$ESP_DIR/idf                      # Or any path you like.
+export ESPTOOLS=$ESP_DIR/xtensa-esp32-elf/bin     # xtensa toolchain
+
+export ESPIDF_HASH=9e70825d1e1cbf7988cf36981774300066580ea7
 
 FIRMWARES=/mnt/c/develop/MyPython/FIRMWARE
-export ESPDIF_HASH=9e70825d1e1cbf7988cf36981774300066580ea7
 
-# function do_build() {
-#     descr=$1
-#     board=$2
-#     shift
-#     shift
-#     echo "building $descr $board"
-#     build_dir=/tmp/stm-build-$board
-#     make -B $@ BOARD=$board BUILD=$build_dir || exit 1
-#     mv $build_dir/firmware.dfu $dest_dir/$descr-$date-$git_tag.dfu
-#     rm -rf $build_dir
-# }
+# Add to path 
+# pathadd path/to/add [after]
+pathadd() {
+    newelement=${1%/}
+    if [ -d "$1" ] && ! echo $PATH | grep -E -q "(^|:)$newelement($|:)" ; then
+        if [ "$2" = "after" ] ; then
+            PATH="$PATH:$newelement"
+        else
+            PATH="$newelement:$PATH"
+        fi
+    fi
+}
+
+# do a build of a port 
+# arg1 - description
+# arg2 - board as defined in esp\board\xxxx
+
+function do_build() {
+    DESCR=$1
+    BOARD=$2
+    DEST=$3
+    shift
+    shift
+    shift
+    echo "building $DESCR $BOARD"
+
+    #temporary build dir 
+    # build_dir=/tmp/stm-build-$board
+    # make -B $@ BOARD=$board BUILD=$build_dir || exit 1
+    # mv $build_dir/firmware.dfu $dest_dir/$descr-$date-$git_tag.dfu
+    # rm -rf $build_dir
+    cd $MPY_DIR/ports/esp32
+    #prepend to path 
+    pathadd $ESPTOOLS
+    
+    # if [ -z ${IDF_TOOLS_INSTALL_CMD+x} ]; then echo "var is unset";  else   echo "var is set to '$IDF_TOOLS_INSTALL_CMD'";  fi
+    # dotsource the ESPDIF environment
+    if [ -z ${IDF_TOOLS_INSTALL_CMD+x} ]; then 
+        source $ESPIDF/export.sh
+    fi
+
+
+
+    make submodules n=4
+    
+    if make BOARD=$BOARD j=4;
+    then 
+        echo -e  "${GREEN}- Build Completed{NC}"
+        cp ./build-$BOARD/firmware.bin $FIRMWARES/${DESCR,,}.bin --verbose
+    else
+        echo -e  "${RED}Error during Build${NC}"
+    fi
+}
 # # build the versions
 # do_build pybv3 PYBV3
 # do_build pybv3-network PYBV3 MICROPY_PY_WIZNET5K=1 MICROPY_PY_CC3K=1
@@ -42,7 +88,7 @@ case "$1" in
         
     2|"toolchain")   
         echo -e  "${CYAN}2. Fetch the required ESP IDF form git using the hash."
-        echo -e "${GREEN}   HASH : $ESPDIF_HASH ${NC}"
+        echo -e "${GREEN}   HASH : $ESPIDF_HASH ${NC}"
 
         if [ ! -d $ESPIDF ]; then
             mkdir -p $ESPIDF
@@ -54,7 +100,7 @@ case "$1" in
         # assume the repo is already there 
         cd $ESPIDF
         # ? should we fetch a recent ?
-        git checkout $ESPDIF_HASH 
+        git checkout $ESPIDF_HASH 
         git submodule update --init --recursive
         ;;     
         
@@ -79,8 +125,8 @@ case "$1" in
         fi
         ;;
     4)  
-        echo -e  "${CYAN}4. Download ESPDIF toolchain ${NC}"
-        echo -e  "${CYAN} [sudo] Install PreReqs${NC}"
+        echo -e  "${CYAN}4. Download ESPIDF toolchain ${NC}"
+        echo -e  "${CYAN} [sudo] Install PreReqs to $ESP_DIR ${NC}"
         # Ubuntu and Debian
         sudo apt-get install gcc git wget make libncurses-dev flex bison gperf python python-pip python-setuptools python-serial python-cryptography python-future python-pyparsing libffi-dev libssl-dev
 
@@ -88,31 +134,22 @@ case "$1" in
         mkdir -p ~/downloads
         cd ~/downloads
         wget https://dl.espressif.com/dl/xtensa-esp32-elf-linux64-1.22.0-80-g6c4433a-5.2.0.tar.gz
-        # Extract 
-        mkdir -p ~/esp
-        cd ~/esp
+        # Extract tar/zip to xtensatools folder
+        mkdir -p $ESP_DIR
+        cd $ESP_DIR
         tar -xzf ~/downloads/xtensa-esp32-elf-linux64-1.22.0-80-g6c4433a-5.2.0.tar.gz
-        #prepend to path 
-        export PATH="$HOME/esp/xtensa-esp32-elf/bin:$PATH"
+        # prepend to path 
+        pathadd $ESPTOOLS
+        export PATH
         ;;
 
-    5)
+    5|"build")
         echo -e  "${CYAN}5. Make ESP32${NC}"
         cd $MPY_DIR
         cd ports/esp32
 
-        BOARD=GENERIC_SPIRAM
-        #prepend to path 
-        export PATH="$HOME/esp/xtensa-esp32-elf/bin:$PATH"
-        make submodules n=4 V=1
-        
-        if make BOARD=$BOARD n=4 V=1;
-        then 
-            echo -e  "${GREEN}- Copy Firmware${NC}"
-            cp ./build-$BOARD/firmware.bin $FIRMWARES/mpy_${BOARD,,}_custom.bin
-        else
-            echo -e  "${RED}Error during Build${NC}"
-        fi
+        # do_build "mpy_jos_esp32_spiram" GENERIC_SPIRAM $FIRMWARES
+        do_build "mpy_josv_esp32_spiram" JOSV_SPIRAM $FIRMWARES
         ;;
     # 6)
     #     echo -e  "${CYAN}5. flash ESP32${NC}"
@@ -137,7 +174,7 @@ case "$1" in
         export PATH="$HOME/esp/xtensa-esp32-elf/bin:$PATH"
         source $MPY_DIR/ports/esp32/build-venv/bin/activate
         make clean
-        if make V=1 MPY_DIR=$MPY_DIR ARCH=xtensawin;
+        if make MPY_DIR=$MPY_DIR ARCH=xtensawin;
         then 
             echo -e  "${GREEN}- Native module created${NC}"
         else
